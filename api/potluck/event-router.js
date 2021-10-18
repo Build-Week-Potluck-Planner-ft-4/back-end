deeeda3e182637dcaae3668068e806a6efd5dca1
Sarah D Herren <herren.sarah@icloud.com>
2021-10-18 16:24:58 -0500

    [POST] authorized register and login

---
+++ api/potluck/event-router.js
@@ -0,0 +1,13 @@
+//[POST] create event
+
+//[PUT] edit event
+
+//[PUT] add food items
+
+//[PUT] assign food items to user
+
+//[GET] fetch potlucks user is assigned to
+
+//[GET] fetch potluck by filter potluck name to see potluck details
+
+//[DELETE] delete event
